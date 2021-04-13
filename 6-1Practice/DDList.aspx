42714c7d59cca8bc9ad064ffe66f8824c3b367eb
MIS803 <MIS803@30803_PC52>
2021-04-13 17:29:50 +0800

    6-1 練習

--- 6-1Practice/DDList.aspx
+++ 6-1Practice/DDList.aspx
@@ -10,6 +10,21 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            <asp:DropDownList ID="ddl_Area" runat="server" OnSelectedIndexChanged="ddl_Area_SelectedIndexChanged" style="height: 19px">
+                <asp:ListItem>北區</asp:ListItem>
+                <asp:ListItem>中區</asp:ListItem>
+            </asp:DropDownList>
+            <asp:DropDownList ID="ddl_Place" runat="server">
+                <asp:ListItem>基隆</asp:ListItem>
+                <asp:ListItem>台北</asp:ListItem>
+                <asp:ListItem>新北</asp:ListItem>
+                <asp:ListItem>苗栗</asp:ListItem>
+                <asp:ListItem>台中</asp:ListItem>
+                <asp:ListItem>南投</asp:ListItem>
+            </asp:DropDownList>
+
+            <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged"></asp:DropDownList>
+            <asp:DropDownList ID="DropDownList2" runat="server"></asp:DropDownList>
         </div>
     </form>
 </body>
--- 6-1Practice/DDList.aspx.cs
+++ 6-1Practice/DDList.aspx.cs
@@ -7,8 +7,36 @@ using System.Web.UI.WebControls;
 
 namespace _6_1Practice {
     public partial class DDList : System.Web.UI.Page {
+        string[,] sa_2D = new string[2, 3] { { "00","01","02"},{ "10", "11", "12" } };
         protected void Page_Load(object sender, EventArgs e) {
+            if (IsPostBack == false)
+            {
+                DropDownList1.Items.Add(new ListItem("0", "0"));
+                DropDownList1.Items.Add(new ListItem("1", "1"));
+            }
 
+        }
+
+        protected void ddl_Area_SelectedIndexChanged(object sender, EventArgs e){
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DropDownList2.Items.Clear();
+            if (DropDownList1.SelectedValue == "1")
+            {
+                for (int i_Ct = 0; i_Ct < 3; i_Ct++)
+                {
+                    DropDownList2.Items.Add(new ListItem(sa_2D[1, i_Ct], sa_2D[1, i_Ct]));
+                }
+            }
+            else {
+                for (int i_Ct = 0; i_Ct < 3; i_Ct++)
+                {
+                    DropDownList2.Items.Add(new ListItem(sa_2D[0, i_Ct], sa_2D[0, i_Ct]));
+                }
+            }
         }
     }
 }
